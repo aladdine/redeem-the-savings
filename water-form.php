dfe4f1145ee71af78335641e8d93f4e8fc8fbe93
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-08-16 09:41:40 -0700

    added water saving calculation

--- water-form.php
+++ water-form.php
@@ -11,30 +11,52 @@
 		<div class="col-md-12">
 
 		<h2>1- Upload Bills:</h2>
+
+		<table class="table">
+		  <tr>
+		  	<th>Bill before savings</th>
+		  	<th>Bill after savings</th>
+		  	<th>Calculated savings </th>
+		  </tr>
+
+		  <tr>
+		  	<td>
+					<form id="air_tickets_form_2">
+					  <div class="form-group-2">
+					    <p><input type="hidden" name="apikey" value="e1c97619-b755-4fb0-8fec-e06d8594962e"></p>
+					    <label for="air_tickets_2">before savings bill</label>
+					    <input class="col-md-3" type="file" id="air_tickets_2" name="file">
+					    <span><input  class="btn-info" value="upload" type="submit"></span>
+					  </div>	  
+				   	</form>
+				   	<div id="this-month-consumption"></div>
+				   
+		  	</td>
+		  	<td>
+		  			<form id="air_tickets_form">
+					  <div class="form-group">
+					    <p><input type="hidden" name="apikey" value="e1c97619-b755-4fb0-8fec-e06d8594962e"></p>
+					    <label for="air_tickets">after savings bill</label>
+					    <input class="col-md-3" type="file" id="air_tickets" name="file">
+					    <span><input  class="btn-info" value="upload" type="submit"></span>
+					  </div>	  
+				   	</form>
+				   	<div id="this-month-consumption-2"></div>
+		  	</td>
+		  	<td> <div id="water-savings"></div> </td>
+		  </tr>
+  
+       </table>
+       <p>Note: Please make sure the bills are for 2 consecutive months.</p>
 		
 
-		<form id="air_tickets_form">
-  
-		  <div class="form-group">
-		    <p><input type="hidden" name="apikey" value="e1c97619-b755-4fb0-8fec-e06d8594962e"></p>
-		    <label for="air_tickets">Upload The After Saving Bill:</label>
-		    <input class="col-md-3" type="file" id="air_tickets" name="file">
-		    <span><input  class="btn-primary" type="submit"></span>
-		  </div>	  
-	   </form>
+		
 
-	     <form id="air_tickets_form_2">
-  
-		  <div class="form-group-2">
-		    <p><input type="hidden" name="apikey" value="e1c97619-b755-4fb0-8fec-e06d8594962e"></p>
-		    <label for="air_tickets_2">Upload The After Saving Bill:</label>
-		    <input class="col-md-3" type="file" id="air_tickets_2" name="file">
-		    <span><input  class="btn-primary" type="submit"></span>
-		  </div>	  
-	   </form>
+	    
 
-          <div id="this-month-consumption"></div>
-          <div id="this-month-consumption-2"></div>
+          
+          
+         
 
 
 
@@ -44,20 +66,7 @@
 		
 		<h2>2- Fill Information:</h2>
 	<div class="row">	
-		<div class="input-group col-md-3">
-	        <span class="input-group-addon" id="basic-addon1">How many people in the property?</span>
-	        <input type="number" class="form-control" value="2" aria-describedby="basic-addon1">
-        </div>
-
-        <div class="input-group col-md-3">
-	        <span class="input-group-addon" id="basic-addon2">Surface Area of the property?</span>
-	        <input type="number" class="form-control" value="2" aria-describedby="basic-addon1">
-        </div>
-
-        <div class="input-group col-md-3">
-	        <span class="input-group-addon" id="basic-addon3">Surface Area of the property?</span>
-	        <input type="number" class="form-control" value="2" aria-describedby="basic-addon1">
-        </div>
+		
 
 	</div>
 
@@ -78,7 +87,7 @@
 
 
 <script>
- $("form#air_tickets_form").submit(function(event){
+ $("form#air_tickets_form_2").submit(function(event){
  
 	   //disable the default form submission
 		event.preventDefault();
@@ -111,16 +120,15 @@
         this_month_consumption = bill_content.match(/ gallons\) Total Water Use Comparison/g);
         consumption = bill_content.indexOf(this_month_consumption[0]);
         consumption = bill_content.substring(consumption - 5, consumption);
-        consumption = Number(consumption.replace(",",""));       
-        console.log("cb: " + check_bill);
-        $("#this-month-consumption").html("<h4>Monthly consumption before saving: " + consumption + " gallons</h4>");
+        consumption_before = Number(consumption.replace(",",""));       
+        $("#this-month-consumption").html("<p class='output'>Monthly consumption before saving: " + consumption_before + " gallons</p>");
         }
     }
   });
 
 });
 
- $("form#air_tickets_form_2").submit(function(event){
+ $("form#air_tickets_form").submit(function(event){
  
 	   //disable the default form submission
 		event.preventDefault();
@@ -151,10 +159,17 @@
         this_month_consumption = bill_content.match(/ gallons\) Total Water Use Comparison/g);
         consumption = bill_content.indexOf(this_month_consumption[0]);
         consumption = bill_content.substring(consumption - 5, consumption);
-        consumption = Number(consumption.replace(",",""));       
+        consumption_after = Number(consumption.replace(",",""));       
         console.log(consumption);
 
-        $("#this-month-consumption-2").html("<h4>Monthly consumption after saving: " + consumption + " gallons</h4>");
+        $("#this-month-consumption-2").html("<p class='output'>Monthly consumption after saving: " + consumption_after + " gallons</p>");
+         
+        
+        savings_gallons = consumption_before - consumption_after;
+
+        savings_percent = savings_gallons / consumption_before * 100; 
+
+         $("#water-savings").html("<p class='output'>Your month to month savings are: " + savings_gallons + " gallons (" + savings_percent + "%)</p>");
     	}
 
     }
